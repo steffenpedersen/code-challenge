1dc7c48d420c2a9abf038dc1f8cb28968aa3fc35
steffenpedersen <steffen.pedersen@live.dk>
2020-04-17 13:39:38 +0200

    Add rendered font from prototype

--- src/views/RenderedFont.vue
+++ src/views/RenderedFont.vue
@@ -1,4 +1,46 @@
 <template>
   <div class="rendered-font">
+    <div class="rendered-font-inputs">
+      <input id="rendered-font-text" type="text" placeholder="Text" />
+      <input id="rendered-font-width" type="text" placeholder="Width" />
+      <input id="rendered-font-height" type="text" placeholder="Height" />
+    </div>
+
+    <div>
+      <select id="rendered-font-fonts">
+        <option value>Font Options</option>
+        <option value="Verdana">Verdana</option>
+        <option value="Ariel">Ariel</option>
+        <option value="Times New Roman">Times New Roman</option>
+      </select>
+
+      <button v-on:click="renderedResultPrototype(this)">Render Text</button>
+    </div>
+
+    <div>
+      <span id="result-rendered-font"></span>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "RenderedFont",
+  methods: {
+    renderedResultPrototype() {
+      const renderedResult = document.getElementById("result-rendered-font");
+      const renderedText = document.getElementById("rendered-font-text").value;
+      const renderedWidth = document.getElementById("rendered-font-width")
+        .value;
+      // const renderedHeight = document.getElementById("rendered-font-height").value;
+      // const renderedFonts = document.getElementById("rendered-font-fonts").value;
+
+      renderedResult.innerHTML = renderedText;
+      renderedResult.style.width = renderedWidth + "px";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
