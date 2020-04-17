fce9274ab90055dda54558e9ff888fe2358c4264
steffenpedersen <steffen.pedersen@live.dk>
2020-04-17 15:46:35 +0200

    Add more styling to rendered font

--- src/views/RenderedFont.vue
+++ src/views/RenderedFont.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="rendered-font">
     <div class="rendered-font-inputs">
-      <input id="rendered-font-text" type="text" placeholder="Text" />
-      <input id="rendered-font-width" type="text" placeholder="Width" />
-      <input id="rendered-font-height" type="text" placeholder="Height" />
+      <label for="rendered-font-text">Text</label>
+      <input id="rendered-font-text" type="text" />
     </div>
 
-    <div>
+    <div class="rendered-font-inputs">
+      <label for="rendered-font-width">Width</label>
+      <input id="rendered-font-width" type="text" />
+    </div>
+
+    <div class="rendered-font-inputs">
+      <label for="rendered-font-height">Height</label>
+      <input id="rendered-font-height" type="text" />
+    </div>
+
+    <div class="rendered-font-inputs">
+      <label for="rendered-font-fonts">Font Options</label>
       <select id="rendered-font-fonts">
-        <option value>Font Options</option>
         <option value="Verdana">Verdana</option>
         <option value="Ariel">Ariel</option>
         <option value="Times New Roman">Times New Roman</option>
       </select>
-
-      <button v-on:click="renderedResultPrototype(this)">Render Text</button>
     </div>
+
+    <button class="rendered-font-inputs rendered-font-inputs--center" v-on:click="renderedResult(this)">Render Text</button>
 
     <div id="result-rendered-font">
       <p id="result-rendered-text"></p>
@@ -29,7 +38,7 @@ import fitty from "fitty";
 export default {
   name: "RenderedFont",
   methods: {
-    renderedResultPrototype() {
+    renderedResult() {
       const renderedFont = document.getElementById("result-rendered-font");
       const renderedResult = document.getElementById("result-rendered-text");
       const renderedText = document.getElementById("rendered-font-text").value;
@@ -44,14 +53,34 @@ export default {
     }
   },
   mounted() {
-    fitty('#result-rendered-text');
+    fitty("#result-rendered-text");
   }
 };
 </script>
 
 <style lang="scss">
+.rendered-font {
+  width: 16em;
+  margin: 0 auto;
+}
+
 #result-rendered-font {
   margin: 0 auto;
+}
+
+.rendered-font-inputs {
+  display: block;
+  margin-bottom: 1em;
+
+  &--center {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  label {
+    margin-right: 0.5em;
+  }
+  
 }
 
 p {
